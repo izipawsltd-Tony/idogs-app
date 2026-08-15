519eb03d27e5be6a37c13ddd84de2af42fcf5303
izipawsltd-Tony <izipawsltd@gmail.com>
2026-08-15 23:39:23 +0930

    feat(billing): expose verified billing summary

---
+++ api/billing-summary.js
@@ -0,0 +1,29 @@
+import Stripe from 'stripe'
+import { initializeApp, getApps, cert } from 'firebase-admin/app'
+import { getAuth } from 'firebase-admin/auth'
+import { getFirestore } from 'firebase-admin/firestore'
+import { createBillingSummaryHandler } from './_lib/billing-handler.js'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+  })
+}
+
+const db = getFirestore()
+
+export default createBillingSummaryHandler({
+  verifyIdToken: token => getAuth().verifyIdToken(token),
+  getProfile: async uid => {
+    const snap = await db.collection('users').doc(uid).get()
+    return snap.exists ? snap.data() : null
+  },
+  retrieveSubscription: id => stripe.subscriptions.retrieve(id, { expand: ['items.data.price'] }),
+  listInvoices: params => stripe.invoices.list(params),
+})
