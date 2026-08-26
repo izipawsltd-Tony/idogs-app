2675a2a18f5070137b55b5fc589931f28ca21a89
izipawsltd-Tony <izipawsltd@gmail.com>
2026-08-26 12:17:30 +0000

    fix(sms): add safe add-on removal

---
+++ api/remove-sms-addon.js
@@ -0,0 +1,27 @@
+import Stripe from 'stripe'
+import { initializeApp, getApps, cert } from 'firebase-admin/app'
+import { getAuth } from 'firebase-admin/auth'
+import { getFirestore } from 'firebase-admin/firestore'
+import { createSmsAddonRemoveHandler } from './_lib/sms-checkout-handler.js'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+  })
+}
+const db = getFirestore()
+
+export default createSmsAddonRemoveHandler({
+  verifyIdToken: token => getAuth().verifyIdToken(token),
+  getProfile: async uid => {
+    const snap = await db.collection('users').doc(uid).get()
+    return snap.exists ? snap.data() : null
+  },
+  retrieveSubscription: id => stripe.subscriptions.retrieve(id, { expand: ['items.data.price'] }),
+  updateSubscription: (id, params) => stripe.subscriptions.update(id, params),
+})
