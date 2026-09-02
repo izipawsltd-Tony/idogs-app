3db693ea93273276cf9d9af1501ef414dde0e567
izipawsltd-Tony <izipawsltd@gmail.com>
2026-09-03 06:59:35 +0930

    diag(billing): log sanitized billing summary failure stage

--- api/billing-summary.js
+++ api/billing-summary.js
@@ -4,6 +4,7 @@ import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
 import { createBillingSummaryHandler } from './_lib/billing-handler.js'
 import { reconcileVerifiedPlusSubscription, verifiedPlusInterval } from './_lib/billing-reconcile.js'
+import { logSanitizedError } from './_lib/http-helpers.js'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -19,18 +20,44 @@ if (!getApps().length) {
 
 const db = getFirestore()
 
+function logBillingStageFailure(stage, error) {
+  logSanitizedError('billing-summary', `BILLING_SUMMARY_${stage}_FAILED`, {
+    message: error?.message,
+    code: error?.code || error?.type,
+  })
+}
+
 export default createBillingSummaryHandler({
   verifyIdToken: token => getAuth().verifyIdToken(token),
   getProfile: async uid => {
     const snap = await db.collection('users').doc(uid).get()
     return snap.exists ? snap.data() : null
   },
-  retrieveSubscription: id => stripe.subscriptions.retrieve(id, { expand: ['items.data.price'] }),
-  listInvoices: params => stripe.invoices.list(params),
+  retrieveSubscription: async id => {
+    try {
+      return await stripe.subscriptions.retrieve(id, { expand: ['items.data.price'] })
+    } catch (error) {
+      logBillingStageFailure('SUBSCRIPTION_RETRIEVE', error)
+      throw error
+    }
+  },
+  listInvoices: async params => {
+    try {
+      return await stripe.invoices.list(params)
+    } catch (error) {
+      logBillingStageFailure('INVOICE_LIST', error)
+      throw error
+    }
+  },
   isSmsConfigured: () => Boolean(process.env.STRIPE_SMS_ADDON_PRICE_ID),
   getSmsPriceId: () => process.env.STRIPE_SMS_ADDON_PRICE_ID || null,
   reconcileVerifiedPlus: async ({ subscription, userId }) => {
     if (!verifiedPlusInterval(subscription)) return false
-    return reconcileVerifiedPlusSubscription({ db, subscription, userId })
+    try {
+      return await reconcileVerifiedPlusSubscription({ db, subscription, userId })
+    } catch (error) {
+      logBillingStageFailure('PLUS_RECONCILE', error)
+      throw error
+    }
   },
 })
