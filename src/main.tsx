1da627f32a358c9519d9a9c9f2e9a2763807a755
izipawsltd-Tony <izipawsltd@gmail.com>
2026-09-19 22:15:27 +0930

    feat(mobile): load mobile safe-area styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './hooks/useAuth'
 import App from './components/App'
 import './index.css'
+import './mobile.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
