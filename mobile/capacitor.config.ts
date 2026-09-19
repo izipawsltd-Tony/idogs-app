2e842970789a2563d93e4a86459b1bfcb4198561
izipawsltd-Tony <izipawsltd@gmail.com>
2026-09-19 22:14:48 +0930

    feat(mobile): configure iDogs app identity

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'au.com.idogs.app',
+  appName: 'iDogs',
+  webDir: '../dist',
+}
+
+export default config
